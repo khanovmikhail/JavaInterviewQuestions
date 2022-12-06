bc82f906991f0dc76f7b482831e4e23171074120
khanovmikhail <johanlgw@gmail.com>
2022-12-07 01:54:02 +0400

    X class task

---
+++ src/com/github/khanovmikhail/X.java
@@ -0,0 +1,10 @@
+package com.github.khanovmikhail;
+
+public class X {
+    static int x;
+
+    public static void main(String[] args) {
+        int y;
+        System.out.println(y = X.x); //What output will be?
+    }
+}
