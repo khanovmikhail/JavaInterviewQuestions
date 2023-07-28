a9b6a9a124188aa1a570f63adb0c0da496dfaa1a
Mikhail Khanov <johanlgw@gmail.com>
2023-07-28 14:41:29 +0400

    Add puzzle2x2

---
+++ src/com/github/khanovmikhail/Puzzle2x2.java
@@ -0,0 +1,11 @@
+package com.github.khanovmikhail;
+
+public class Puzzle2x2 {
+    public static void main(String[] args) {
+        Puzzle2x2.puzzle2x2();
+    }
+
+    private static void puzzle2x2() {
+        System.out.println("2 + 2 = " + 2 + 2);
+    }
+}
